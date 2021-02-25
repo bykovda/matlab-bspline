8fc135880b42910da5fcaeae10c1b090c1c29228
bykovda <75255949+bykovda@users.noreply.github.com>
2021-02-25 16:38:10 +0400

    Update spline2_basis.m

--- spline2_basis.m
+++ spline2_basis.m
@@ -1,4 +1,4 @@
-function [ B, bspline_x, bspline_y] = spline2_basis( x, y, d, knots_x, knots_y)
+function [bspline_x, bspline_y, B] = spline2_basis( x, y, d, knots_x, knots_y)
 
 if nargin < 7
 	lambda = 0;
@@ -17,7 +17,6 @@ ty = [repmat(ymin, [1, d]), knots_y, repmat(ymax, [1, d])];
 
 ncoeff_x = numel(knots_x) + d - 1;
 ncoeff_y = numel(knots_y) + d - 1;
-B = zeros(numel(x), ncoeff_x*ncoeff_y);
 
 %{
 bspline_x = zeros(numel(x), ncoeff_x);
@@ -34,16 +33,13 @@ bspline_x = bspline_v2( x, ncoeff_x, d, tx);
 bspline_y = bspline_v2( y, ncoeff_y, d, ty);
 
 if nargout == 3
-
-
+	B = zeros(numel(x), ncoeff_x*ncoeff_y);
 	for j = 1 : ncoeff_x
 		for k = 1 : ncoeff_y
 			B(:, (j - 1)*ncoeff_y + k) = bspline_x(:, j).*bspline_y(:, k);
 		end
 	end
-else
-	B=[]
-end;
+end
 	
 
 
